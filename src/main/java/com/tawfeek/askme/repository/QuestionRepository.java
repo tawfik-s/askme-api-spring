d9210136e913dff76c9201970c98c538748b7a90
Tawfik  Shalash <74143032+tawfik-s@users.noreply.github.com>
2023-02-13 11:40:33 +0200

    add repositories with full crud operations

---
+++ src/main/java/com/tawfeek/askme/repository/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.tawfeek.askme.repository;
+
+import com.tawfeek.askme.entity.Question;
+import com.tawfeek.askme.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question,Long> {
+    List<Question> findByRecipient(User recipient);
+    List<Question> findBySender(User sender);
+}
