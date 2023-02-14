ebec438b155f86d53862ab5c31cbadf2d8e4bdad
Tawfik  Shalash <74143032+tawfik-s@users.noreply.github.com>
2023-02-14 15:04:35 +0200

    fix a bug and add jpql query

--- src/main/java/com/tawfeek/askme/repository/QuestionRepository.java
+++ src/main/java/com/tawfeek/askme/repository/QuestionRepository.java
@@ -3,6 +3,8 @@ package com.tawfeek.askme.repository;
 import com.tawfeek.askme.entity.Question;
 import com.tawfeek.askme.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,7 @@ import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Long> {
     List<Question> findByRecipient(User recipient);
-    List<Question> findBySender(User sender);
+
+    @Query("select question from Question question where question.sender.id =:id")
+    List<Question> findBySender(@Param("id") Long userId);
 }
